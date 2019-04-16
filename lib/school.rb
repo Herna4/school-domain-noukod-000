34f2d3a1b7520305eaf890341db325974bcd3e8a
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-16 01:45:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+  
+  def initialize(name)
+    @name=name
+    @roster ={}
+  end
+  
+  
+  def roster
+    @roster 
+  end
+  
+  def add_student(name, grade)
+    
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+   
+  end
+  
+  def grade
+  end
+
+  
+end
+
+# school = School.new("Bayside High School")
+# school.roster
