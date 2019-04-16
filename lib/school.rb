bae92f6599f0d1a273a1706da393e6b4ae94bf06
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-16 01:51:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -21,7 +21,14 @@ class School
    
   end
   
-  def grade
+ def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
   end
 
   
